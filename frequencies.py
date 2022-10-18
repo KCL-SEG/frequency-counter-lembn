dbae82d07009a923024e2b11a7629302059f861a
Lemuel Bodi-Ngwala <lembn@outlook.com>
2022-10-18 15:27:13 +0100

    attempt 1

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,14 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def frequencies(items):
+    items_str = [str(item) for item in items]
     frequencies = {}
-    # Your code goes here
+    for item in items_str:
+        if item not in frequencies:
+            frequencies[item] = 1
+        else:
+            frequencies[item] += 1
+
     return frequencies
